da698cddc9c5dac93ab8b85c47c99adc329442d4
GLOIRE MUTALIKO <mufunyig@gmail.com>
2022-07-08 15:43:30 +0200

    feat: validation de formulaire

--- src/components/home/Form.jsx
+++ src/components/home/Form.jsx
@@ -4,28 +4,64 @@ import '../../styles/Form.css'
 
 function Form({ hundleSubmit }) {
     const [nameValue, setNameValue] = useState("")
-    const [mailValue, setMailValue] = useState("")
+    const [mailValue, setMalilValue] = useState("")
+
+    function isValidNom() {
+        const validName = nameValue.trim().length
+        const erreurNom = validName === 0 ?
+            'Renseignez le nom' :
+            validName < 2 ?
+                'Entrer un nom valide' : null
+        document.getElementById('errorNom').textContent = erreurNom
+        return validName >= 2 ? true : false
+
+    }
+
+    function isValidMail() {
+        const validMail = mailValue.trim().length
+        const mailTest = new RegExp(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+")){2,}@((\[[0-9]\.[0-9]{2,}\.[0-9]{2,}\.[0-9]\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)
+
+        const erreurMail = validMail == 0 ?
+            'Renseignez votre mail' : mailTest.test(validMail) ?
+                '' : 'Mail ivalide'
+        document.getElementById('errorMail').textContent = erreurMail
+        return mailTest.test(mailValue) ? true : false
+
+    }
 
     return (
         <form className="home-form">
+            <label>Nom</label>
             <input
                 className="text-fields"
                 value={nameValue}
                 type="text"
                 placeholder="Entrez votre nom"
-                onChange={(e) => setNameValue(e.target.value)}
+                onChange={(e) => {
+                    setNameValue(e.target.value)
+
+                }}
             />
+            <span id="errorNom"></span>
+            <label>E-mail</label>
             <input
                 className="text-fields"
+                value={mailValue}
                 type="text"
                 placeholder="Entrez votre mail"
-                required
+                onChange={(e) => {
+                    setMalilValue(e.target.value)
+                }}
             /><br />
+            <span id="errorMail"></span>
             <input
                 value="Commencer"
                 className="bouton"
                 type="submit"
-                onClick={() => hundleSubmit()}
+                onClick={(e) => {
+                    e.preventDefault();
+                    isValidNom() && isValidMail() ? hundleSubmit() : isValidMail()
+                }}
             />
         </form>
 
